0d4d3e9341123871c51b58074e5ef28af9a40beb
Ercument5038 <ercu2810@gmail.com>
2025-08-25 14:30:38 +0200

    day02 part1

--- 2024/day02/part1.js
+++ 2024/day02/part1.js
@@ -1,7 +1,8 @@
+import { checkPrimeSync } from "crypto";
 import fs from "fs";
 
 // get content of file
-const Data = fs.readFileSync("demo-input.txt");
+const Data = fs.readFileSync("input.txt");
 
 // print content of file
 // console.log(Data.toString());
@@ -12,25 +13,33 @@ const lines = Data.toString().trim().split(/\n/);
 // Split each row into numbers
 const rows = lines.map((line) => line.trim().split(/\s+/).map(Number));
 
-let isSafe;
 let sumSafe = 0;
 
-for (let i = 0; i < rows.length - 1; i++) {
-  const level = rows[i];
-  let isIncreasing;
-
-  if (level > rows[i + 1]) {
-    isIncreasing = false;
-  } else {
-    isIncreasing = true;
+// check if an array is increasing or decreasing
+function CheckSafeReport(num) {
+  // get direction (inc, dec)
+  let num_dir = num[1] - num[0];
+  // if result of num_dir * diff is negative means it isnt strictly inscreasing
+  // or decreasing
+  for (let i = 0; i < num.length - 1; i++) {
+    if (num_dir * (num[i + 1] - num[i]) <= 0) {
+      return false;
+    }
   }
 
-  let diff = Math.abs(level - rows[i + 1]);
-  if (diff <= 1 && diff >= 3) {
-    isSafe = true;
+  // if differencee is less then 1 or greaten then 3 is is not safe
+  for (let i = 0; i < num.length - 1; i++) {
+    let diff = Math.abs(num[i] - num[i + 1]);
+    if (diff < 1 || diff > 3) {
+      return false;
+    }
+  }
+  return true;
+}
+
+for (let i = 0; i < rows.length; i++) {
+  if (CheckSafeReport(rows[i])) {
     sumSafe++;
-  } else {
-    isSafe = false;
   }
 }
 
